e8e5ad8987c0cebecda803eff600c8390582f331
deepak179-s <deepak179-s@users.noreply.github.com>
2026-04-01 18:57:24 +0530

    refactor: share the app shell across web and electron

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  isElectron: true,
   platform: process.platform,
   versions: process.versions
 });
